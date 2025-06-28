9b8c86580d93be6db050f451cf93b92ed36b69c1
Ebenezer Bakouan <bakouanebenezer01@gmail.com>
2025-06-28 20:33:43 +0000

    final

--- app.py
+++ app.py
@@ -569,7 +569,6 @@ else:
 st.markdown("---")
 st.markdown("""
 <div style="text-align: center; color: #666; padding: 20px;">
-    <p>Dashboard développé avec ❤️ par [Votre Nom] - © 2023</p>
-    <p>Technologies: Streamlit • Scikit-learn • XGBoost • Plotly</p>
+    <p>Dashboard développé par BAKOUAN Y.JD.E - © 2025</p>
 </div>
 """, unsafe_allow_html=True)
